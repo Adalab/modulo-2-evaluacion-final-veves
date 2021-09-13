4d77ea182074a80364cfd757c4f445eeea30866c
Veves <vevespm@gmail.com>
2021-09-13 10:39:03 +0200

    add favs

--- js/main.js
+++ js/main.js
@@ -1,34 +1,98 @@
-'use strict'
-const search= document.querySelector(".js-search");
-const movieDiv= document.querySelector(".searched-results");
+"use strict";
+const search = document.querySelector(".js-search");
+const movieDiv = document.querySelector(".searched-results");
+const favDiv = document.querySelector(".favorites-column");
 
-let movies= [];
-function showMovies(){
-    let html='';
-    for( const movie of movies){
-        html+= `<div class="searched-film">`
-        if(movie.show.image===null){
-            html+=`<div class="image" style="background-image: url(https://via.placeholder.com/210x295/ffffff/666666/?text=TV);"></div>`
-        }
-        else{
-            html+=`<div class="image" style="background-image: url(${movie.show.image.medium});"></div>`
-        }
-        html+=`<p class="title">${movie.show.name}</p>`
-        html+=`</div>`
-        // <div class="searched-film">
-        //         <div class="image"></div>
-        //         <p class="title">La casa de papel</p>
-        //     </div> background-image: url("paper.gif"); https://via.placeholder.com/210x295/ffffff/666666/?text=TV
-        
-    }
-    movieDiv.innerHTML=html;
-    console.log("aquí va la creación de los divs");
-}
-function handlesearch(){
-const movie = document.querySelector(".movie").value;
+let movies = [];
+let favMovies = [];
+
+//escucha el botón de búsqueda
+search.addEventListener("click", handlesearch);
+//busca en la API
+function handlesearch() {
+  const movie = document.querySelector(".movie").value;
   fetch(`//api.tvmaze.com/search/shows?q=${movie}`)
-  .then(response => response.json())
-  .then((data) => movies=data)
-  .then(data => showMovies());
+    .then((response) => response.json())
+    .then((data) => (movies = data))
+    .then((_data) => showMovies());
 }
-search.addEventListener('click', handlesearch);
+//pinta el html de los resultados de la búsqueda
+function showMovies() {
+  let favClass = '';
+  let html = "";
+  for (const movie of movies) {
+    const isFav = isFavorite(movie);
+  if (isFav) {
+    favClass = 'fav-result';
+  } else {
+    favClass = '';
+  }
+    html += `<div class="searched-film  ${favClass}" id="${movie.show.id}">`;
+    if (movie.show.image === null) {
+      html += `<div class="image" style="background-image: url(https://via.placeholder.com/210x295/ffffff/666666/?text=TV);"></div>`;
+    } else {
+      html += `<div class="image" style="background-image: url(${movie.show.image.medium});"></div>`;
+    }
+    html += `<p>${movie.show.name}</p>`;
+    html += `</div>`;
+  }
+  movieDiv.innerHTML = html;
+
+  listen();
+  //listenFav();
+}
+
+
+
+//maneja el fav/unfav
+function handleFavs(ev) {
+  const selectedMovie = ev.currentTarget.id;
+  const objetClicked = movies.find((movie) => {
+    return movie.show.id == selectedMovie;
+    
+  });
+  const favoritesFound = favMovies.findIndex((fav) => {
+    return fav.show.id == selectedMovie;
+  });
+  if (favoritesFound === -1) {
+    favMovies.push(objetClicked);
+  } else {
+    favMovies.splice(favoritesFound, 1);
+  }
+  setInLocalStorage();
+  showMovies();
+  showFavs();
+}
+function showFavs(){
+  let html='';
+  for (const fav of favMovies){
+    html+=`<div class="favorite-div" id="${fav.show.id}">`
+    html+=`<p class="fav-title">${fav.show.name}"</p>`
+    if (fav.show.image === null) {
+      html += `<div class="fav-image" style="background-image: url(https://via.placeholder.com/210x295/ffffff/666666/?text=TV);"></div>`;
+    } else {
+      html += `<div class="fav-image" style="background-image: url(${fav.show.image.medium});"></div>`;
+    }
+    html+=`</div>`
+  }
+  favDiv.innerHTML= html;
+}
+
+function isFavorite(movie) {
+  //comprueba si la película está en los favoritos
+  const favoriteFound = favMovies.find((fav) => {
+    return fav.show.id === movie.show.id;
+  });
+  if (favoriteFound === undefined) {
+    return false;
+  } else {
+    return true;
+  }
+}
+//escucha al marcar una pelicula
+function listen() {
+    const movieDivs = document.querySelectorAll(".searched-film");
+    for (const favDiv of movieDivs) {
+      favDiv.addEventListener("click", handleFavs);
+    }
+  }
